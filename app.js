0ec4f753ad44775a35e6fae7da9b7d294ea07269
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-07-10 09:04:29 +0530

    Add DB

--- app.js
+++ app.js
@@ -1,46 +1,76 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
+app.set("view engine", "ejs");
 
-const date = require(__dirname + "/date.js");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemsSchema = {
+  name: String,
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+// 3 default Items
+// const exercise = new Item({
+//   name: "Exercise",
+// });
+
+// const read = new Item({
+//   name: "Reading Book",
+// });
+
+// const code = new Item({
+//   name: "Continue the course",
+// });
+
+// const defaultItems = [exercise, read, code];
+
+// Item.insertMany(defaultItems, function (err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Default items added successfully");
+//   }
+// });
 
 const items = [];
 const workItems = [];
 
-app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(express.static("public"))
-
-app.get("/", function(req, res) {
-
-    let day = date.getDate();
-
-    res.render('list', {listTitle: day, newListItems: items});
-
-});
-
-app.post("/", function(req, res){
-
-    let item = req.body.newItem;
-
-    if(req.body.list === "Work"){
-        workItems.push(item);
-        res.redirect("/work");
+app.get("/", function (req, res) {
+  Item.find({}, function (err, results) {
+    if (err) {
+      console.log(err);
     } else {
-        items.push(item)
-        res.redirect("/");
+      res.render("list", { listTitle: "Today", newListItems: results });
     }
-    
-})
-
-app.get("/work", function(req, res){
-    res.render('list', {listTitle: "Work List", newListItems: workItems});
+  });
 });
 
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Server started on port 3000");
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server started on port 3000");
 });
