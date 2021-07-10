b8874beb8d909ff25b902c5e4c8aafa9bd585feb
Aditya Rajguru <rajguru.aditya.25@gmail.com>
2021-07-10 15:52:42 +0530

    Add mongo server

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose.connect(
+  "mongodb+srv://admin-aditya:aditya221b@cluster0.pwqlc.mongodb.net/todolistDB",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const itemsSchema = {
   name: String,
